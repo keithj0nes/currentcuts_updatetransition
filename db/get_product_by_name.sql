bdbb77678928bce94e4995572071438972ebf890
keith <keith@raincityambience.com>
2017-04-15 01:02:19 -0700

    search updated with correct sql query

--- db/get_product_by_name.sql
+++ db/get_product_by_name.sql
@@ -1,1 +1,8 @@
-SELECT * FROM products WHERE UPPER(name) LIKE UPPER($1);
+-- SELECT * FROM products WHERE UPPER(name) LIKE UPPER($1);
+
+
+SELECT DISTINCT on (products.id) products.*, prices.price FROM products
+INNER JOIN product_price_size ON products.id = product_price_size.productId
+INNER JOIN prices ON prices.id = product_price_size.priceId
+WHERE UPPER(name) LIKE UPPER($1)
+ORDER BY products.id, prices.price;
--- public/js/controllers/searchController.js
+++ public/js/controllers/searchController.js
@@ -1,27 +1,18 @@
 angular.module("ccvApp").controller("searchController", function($scope, $stateParams, mainService){
 
 
-  console.log($stateParams, "state params search");
-  console.log($stateParams.search);
-var searchTerm = $stateParams.search;
+  var searchTerm = $stateParams.search;
 
-$scope.test = "HEY";
+  mainService.getProductByName(searchTerm).then(function(response){
+    $scope.searchProducts = response;
+    console.log(response, "response in controller");
+    console.log($scope.searchProducts, "scope search prodcutz");
 
-console.log(searchTerm, "searchTerm");
-
-    mainService.getProductByName(searchTerm).then(function(response){
-      $scope.searchProducts = response;
-      console.log(response, "response in controller");
-      console.log($scope.searchProducts, "scope search prodcutz");
-    })
-
-
-
-
-
-  setTimeout(function () {
-    console.log($scope.searchProducts);
-
-  }, 4000);
+    if(response.length >= 1){
+      $scope.search = true;
+    } else {
+      $scope.search = false
+    }
+  })
 
 })
