99b904a1302eacd119a60aad78b14e44f2c6c9fc
keith <keith@raincityambience.com>
2017-09-02 02:07:55 -0700

    contact form functionality with css working

--- public/dist/js/bundle.js
+++ public/dist/js/bundle.js
@@ -5048,7 +5048,65 @@ angular.module("ccvApp").controller("categoryController", function ($scope, $sta
 
 angular.module("ccvApp").controller("contactController", function ($scope) {
 
-  $scope.test = "testing123";
+  $scope.clearInput = function (model) {
+    console.log(model);
+    switch (model) {
+      case 'contactNameFirst':
+        $scope.contactNameFirstR = false;break;
+      case 'contactEmail':
+        $scope.contactEmailR = false;break;
+      case 'contactSubject':
+        $scope.contactSubjectR = false;break;
+      case 'contactMessage':
+        $scope.contactMessageR = false;break;
+    }
+  };
+
+  $scope.contactFormSubmit = function (fname, lname, email, subject, message) {
+
+    $scope.contactNameFirstR = false;
+    $scope.contactEmailR = false;
+    $scope.contactSubjectR = false;
+    $scope.contactMessageR = false;
+
+    if (!fname) {
+      $scope.contactNameFirstR = true;
+    }
+    if (!email) {
+      $scope.contactEmailR = true;
+    }
+    if (!subject) {
+      $scope.contactSubjectR = true;
+    }
+    if (!message) {
+      $scope.contactMessageR = true;
+    }
+
+    if ($scope.contactNameFirstR === true || $scope.contactEmailR === true || $scope.contactSubjectR === true || $scope.contactMessageR === true) {
+      console.log("not storing data yet");
+    } else {
+
+      var contactData = {
+        fname: fname,
+        lname: lname,
+        email: email,
+        subject: subject,
+        message: message
+      };
+      console.log(contactData);
+
+      //clear form after submit to backend
+      $scope.contactNameFirst = "";
+      $scope.contactNameLast = "";
+      $scope.contactEmail = "";
+      $scope.contactSubject = "";
+      $scope.contactMessage = "";
+
+      setTimeout(function () {
+        alert("SUCCESS!");
+      }, 100);
+    }
+  };
 });
 "use strict";
 
--- public/js/controllers/contactController.js
+++ public/js/controllers/contactController.js
@@ -1,4 +1,55 @@
 angular.module("ccvApp").controller("contactController", function($scope){
 
-  $scope.test = "testing123"
+
+  $scope.clearInput = function(model){
+    console.log(model);
+    switch (model) {
+      case 'contactNameFirst': $scope.contactNameFirstR = false; break;
+      case 'contactEmail': $scope.contactEmailR = false; break;
+      case 'contactSubject': $scope.contactSubjectR = false; break;
+      case 'contactMessage': $scope.contactMessageR = false; break;
+    }
+  }
+
+
+  $scope.contactFormSubmit = function(fname, lname, email, subject, message){
+
+    $scope.contactNameFirstR = false;
+    $scope.contactEmailR = false;
+    $scope.contactSubjectR = false;
+    $scope.contactMessageR = false;
+
+    if(!fname){$scope.contactNameFirstR = true;}
+    if(!email){$scope.contactEmailR = true;}
+    if(!subject){$scope.contactSubjectR = true;}
+    if(!message){$scope.contactMessageR = true;}
+
+    if($scope.contactNameFirstR === true ||
+       $scope.contactEmailR === true ||
+       $scope.contactSubjectR === true ||
+       $scope.contactMessageR === true){
+         console.log("not storing data yet");
+    } else {
+
+      const contactData = {
+        fname,
+        lname,
+        email,
+        subject,
+        message
+      }
+      console.log(contactData);
+
+     //clear form after submit to backend
+      $scope.contactNameFirst = "";
+      $scope.contactNameLast = "";
+      $scope.contactEmail = "";
+      $scope.contactSubject = "";
+      $scope.contactMessage = "";
+
+      setTimeout(function(){
+        alert("SUCCESS!");
+      },100)
+    }
+  }
 })
