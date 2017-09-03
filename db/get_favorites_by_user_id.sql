258ce60ee5e9923524f40d700dec4ad49946467b
keith <keith@raincityambience.com>
2017-09-03 15:55:47 -0700

    favorites now show price and not archived products

--- db/get_favorites_by_user_id.sql
+++ db/get_favorites_by_user_id.sql
@@ -1,3 +1,6 @@
-SELECT products.name, products.img1, products.id FROM favorites
-INNER JOIN products ON favorites.product_id = products.id
-WHERE user_id = $1 ORDER BY favorites.id ASC
+SELECT DISTINCT on (favorites.id) products.*, prices.price FROM products
+INNER JOIN product_price_size ON products.id = product_price_size.productId
+INNER JOIN prices ON prices.id = product_price_size.priceId
+INNER JOIN favorites ON favorites.product_id = products.id
+WHERE user_id = $1 AND (products.archived IS NULL OR products.archived = false)
+ORDER BY favorites.id ASC, prices.price
