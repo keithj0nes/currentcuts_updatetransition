ecaf0073ba028edd616eedd860902db0b9d3a1c1
keith <keith@raincityambience.com>
2017-10-15 17:19:09 -0700

    update search sql to return only active products

--- db/get_product_by_name.sql
+++ db/get_product_by_name.sql
@@ -4,5 +4,5 @@
 SELECT DISTINCT on (products.id) products.*, prices.price FROM products
 INNER JOIN product_price_size ON products.id = product_price_size.productId
 INNER JOIN prices ON prices.id = product_price_size.priceId
-WHERE UPPER(name) LIKE UPPER($1)
+WHERE UPPER(name) LIKE UPPER($1) AND active = true AND (archived IS NULL OR archived = false)
 ORDER BY products.id, prices.price;
